ae6adcdfe1c6b685853b450e579dfcfe973d28ec
Zuna <tales.of.zuna@gmail.com>
2023-03-28 18:01:18 +0800

    3:35.23 pagination sorting limitation

--- controller/productCtrl.js
+++ controller/productCtrl.js
@@ -1,5 +1,6 @@
 const Product = require("../models/productModel");
 const asyncHandler = require("express-async-handler");
+const match = require("nodemon/lib/monitor/match");
 const slugify = require("slugify");
 const createProduct = asyncHandler(async (req, res) => {
   try {
@@ -49,13 +50,41 @@ const getaProduct = asyncHandler(async (req, res) => {
 
 const getAllProduct = asyncHandler(async (req, res) => {
   try {
+    // filter
     const queryObj = { ...req.query };
     const excludeFields = ["page", "sort", "limit", "fields"];
-
-    let queryObj;
     excludeFields.forEach((el) => delete queryObj[el]);
-    const getallProducts = await Product.find(req.query);
-    res.json(getallProducts);
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    let query = Product.find(JSON.parse(queryStr));
+
+    //sortings
+
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(",").join(" ");
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort("-createdAt");
+    }
+
+    // limiting the fields
+    if (req.query.fields) {
+      const fields = req.query.fields.split(",").join(" ");
+      query = query.select(fields);
+    } else {
+      query = query.select("-__v");
+    }
+
+    //pagination
+    const page = req.query.page;
+    const limit = req.query.limit;
+    const skip = (page - 1) * limit;
+    query = query.skip(skip).limit(limit);
+
+    console.log(page, limit, skip);
+
+    const product = await query;
+    res.json(product);
   } catch (error) {
     throw new Error(error);
   }
@@ -68,3 +97,5 @@ module.exports = {
   updateProduct,
   deleteProduct,
 };
+
+// 3:35.23 hurtel hiiv
