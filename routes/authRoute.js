094f3047c63e3d61d570f909f400ca38b7314eff
zuna <tales.of.zuna@gmail.com>
2023-04-11 18:09:39 +0800

    backend done

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -18,6 +18,12 @@ const {
   getWishlist,
   saveAddress,
   userCart,
+  getUserCart,
+  emptyCart,
+  applyCoupon,
+  createOrder,
+  getOrders,
+  updateOrderStatus,
 } = require("../controller/userCtrl");
 
 const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
@@ -26,11 +32,17 @@ router.post("/register", createUser);
 router.post("/forgot-password-token", forgotPasswordToken);
 router.put("/reset-password/:token", resetPassword);
 router.put("/password", authMiddleware, updatePassword);
+router.put("/update-order/:id", authMiddleware, isAdmin, updateOrderStatus);
 router.post("/login", loginUserCtrl);
 router.post("/admin-login", loginAdmin);
-router.post("/cart", userCart);
+router.post("/cart", authMiddleware, userCart);
+router.post("/cart/applycoupon", authMiddleware, applyCoupon);
+router.post("/cart/cash-order", authMiddleware, createOrder);
 router.get("/wishlist", authMiddleware, getWishlist);
+router.get("/cart", authMiddleware, getUserCart);
+router.delete("/empty-cart", authMiddleware, emptyCart);
 router.get("/all-users", getallUser);
+router.get("/get-orders", authMiddleware, getOrders);
 router.get("/:id", authMiddleware, isAdmin, getaUser);
 router.get("/refresh", handleRefreshToken);
 router.get("/logout", logout);
